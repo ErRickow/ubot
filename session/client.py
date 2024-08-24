9ebc25fa44a7a176e273b9f6104d85a03b9f8429
Just R <remixingmagelang@gmail.com>
2024-08-24 08:27:10 +0700

    update session/client.py

--- session/client.py
+++ session/client.py
@@ -9,7 +9,7 @@ from ..database._var import Var, Database
 from ..versions import __version__
 import os
 from .classstring import *
-from .._misc.client import ErSession
+from .._misc.client import ErUbotSession
 from .._misc.botclient import ErUserbotToken
 from ..database import pyDatabase
 DB = pyDatabase()
